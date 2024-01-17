5ef9e93cc28d0c1eb7e1e217e85d8e66c368ab7b
Kawther naji <kawthernaji231@gmail.com>
2024-01-17 16:25:42 -0500

    website3

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+    const nav = document.querySelector('nav ul');
+    nav.classList.toggle('active');
+}
+document.querySelector('.menu-toggle').addEventListener('click', toggleMenu);
+
+function handleFormSubmit(event) {
+    event.preventDefault(); 
+    alert('Thank you for contacting us!');
+}
+
+const contactForm = document.querySelector('#contact-form');
+if (contactForm) {
+    contactForm.addEventListener('submit', handleFormSubmit);
+}
